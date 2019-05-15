dbbc6d56b0b17dfa5aa70b65fdb3e9108579d6b8
C-Shi <cshi@ualberta.ca>
2019-05-15 08:33:24 +0800

    update email package

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser')
-var async = require('async');
-var nodemailer = require('nodemailer');
-var crypto = require('crypto');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.set("view engine", "ejs");
@@ -24,36 +25,29 @@ app.get('/contact', (req, res) => {
     res.render("contact");
 })
 
-
 app.post('/contact', (req, res) => {
-  console.log(req.body)
-  let smtpTransport = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-          user: "kyleshi82@gmail.com",
-          pass: process.env.GMAILPW
-        }
-      });
-  let mailOptions = {
-        from: "kyleshi82@gmail.com", // sender address
-        to: req.body.email, // list of receivers
-        subject: 'Message from ' + req.body.name + " about BUSINESS", // Subject line
-        text: req.body.message + "\n\n" + "Please reply to " + req.body.email , // plain text body
-    };
+  const email = req.body.email;
+  const contact = req.body.contact;
+  const message = req.body.message;
 
-    // send mail with defined transport object
-  smtpTransport.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-      res.redirect('/')
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  });
+  const msg = {
+    to: process.env.ADMIN_EMAIL,
+    from: email,
+    subject: `Email from ${contact} at ${email}`,
+    text: message,
+    html: `<p>${message}</p>`
+  }
+
+  sgMail.send(msg)
+  .then(() => {
+    return res.send('Your Message Has Been Sent To CHENG SHI!');
+  })
+  .catch(error => {
+    return res.send(error.message);
+  })
+
 })
+
 
 app.listen(PORT, function(){
     console.log('server start');
